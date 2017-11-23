fdfa41a44b84f07cfe02279061207b45e411e870
shivanXI <shivanstar7@gmail.com>
2017-11-23 17:07:13 +0530

    Added js based initializer for visualisation of output-02

--- code.js
+++ code.js
@@ -21,6 +21,36 @@ var greyColor = '#666';
 var filterActive = false;
 var filterTimeout = false;
 
+
+function highlightNodes(nodeFilter) {
+  graph.iterNodes(function(n){
+    if (nodeFilter(n)) {
+      n.color = n.attr['grey'] ? n.attr['true_color'] : n.color;
+      n.attr['grey'] = 0;
+      n['forceLabel'] = 1;
+    }
+    else if(!n.attr['grey']) {
+      n.attr['true_color'] = n.color;
+      n.color = greyColor;
+      n.attr['grey'] = 1;
+      n['forceLabel'] = -1;
+    }
+  }).draw(2,2,2);
+}
+
+
+function resetHighlight() {
+  graph.iterEdges(function(e){
+    e.color = e.attr['grey'] ? e.attr['true_color'] : e.color;
+    e.attr['grey'] = 0;
+  }).iterNodes(function(n){
+    n.color = n.attr['grey'] ? n.attr['true_color'] : n.color;
+    n.attr['grey'] = 0;
+    n['forceLabel'] = 0;
+  }).draw(2,2,2);
+}
+
+
 graph.bind('overnodes', function(event) {
   var nodes = event.content;
   var neighbors = {};
@@ -28,3 +58,31 @@ graph.bind('overnodes', function(event) {
   if (filterActive) {
     return;
   }
+
+
+$('body').css({cursor: 'pointer'});
+  graph.iterEdges(function(e){
+    if(nodes.indexOf(e.source)<0 && nodes.indexOf(e.target)<0){
+      if(!e.attr['grey']){
+        e.attr['true_color'] = e.color;
+        e.color = greyColor;
+        e.attr['grey'] = 1;
+      }
+    }else{
+      e.color = e.attr['grey'] ? e.attr['true_color'] : e.color;
+      e.attr['grey'] = 0;
+
+      neighbors[e.source] = 1;
+      neighbors[e.target] = 1;
+    }
+  });
+  highlightNodes(function(n) {
+    return neighbors[n.id];
+  });
+}).bind('outnodes',function(){
+  if (filterActive) {
+    return;
+  }
+  $('body').css({cursor: 'default'});
+  resetHighlight();
+});
