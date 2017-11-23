97f040fc7a460ab890c1805bf5f7df2c12a14556
shivanXI <shivanstar7@gmail.com>
2017-11-23 16:41:39 +0530

    Added matrix2latex software code whiuch open source in nature

--- matrix2latex/fixEngineeringNotation.py
+++ matrix2latex/fixEngineeringNotation.py
@@ -14,6 +14,28 @@ You should have received a copy of the GNU General Public License
 along with matrix2latex. If not, see <http://www.gnu.org/licenses/>.
 """
 
-__all__ = ['matrix2latex']
+import re
 
-from matrix2latex import matrix2latex
+def fix(s, table=False):
+    """
+    input: (string) s
+    output: (string) s
+    takes any number in s and replaces the format
+    '8e-08' with '8\e{-08}'
+    """
+    i = re.search('e[-+]\d\d', s)
+    while i != None:
+        before = s[:i.start()]
+        number = s[i.start()+1:i.start()+4]
+        after = s[i.end():]
+#       print 'before', before
+# 	print 'number', number
+# 	print 'after', after
+        if table:
+            num = "%(#)+03d" % {'#': int(number)}
+        else:
+            num = "%(#)3d" % {'#': int(number)}
+            
+        s = '%s\\e{%s}%s' % (before, num, after)
+        i = re.search('e[-+]\d\d', s)
+    return s
